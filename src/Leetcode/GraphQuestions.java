3163c96934ab55aa451fa961eed0e1aeea79a5a5
Kaniel <kankoh98@gmail.com>
2024-06-30 22:15:38 +0800

    695 and Walls & Gates

--- src/Leetcode/GraphQuestions.java
+++ src/Leetcode/GraphQuestions.java
@@ -74,7 +74,7 @@ public class GraphQuestions {
         return count;
     }
 
-    public void numIslandsDfs(char[][] grid, int i, int j) {
+    private void numIslandsDfs(char[][] grid, int i, int j) {
         if (
                 i < 0 ||
                         j < 0 ||
@@ -89,6 +89,71 @@ public class GraphQuestions {
         numIslandsDfs(grid, i, j + 1);
         numIslandsDfs(grid, i - 1, j);
         numIslandsDfs(grid, i, j - 1);
+    }
+
+    // * 695. Max Area of Island
+    // * Time complexity: o(n)
+    // * Time complexity: o(n)
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxValue = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                maxValue = Math.max(maxAreaIslandDfs(grid, i, j), maxValue);
+            }
+        }
+        return maxValue;
+    }
+
+    private int maxAreaIslandDfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+            return 0; // Don't count the current grid;
+        }
+        grid[i][j] = 0;
+        return 1 + maxAreaIslandDfs(grid, i + 1, j) + maxAreaIslandDfs(grid, i, j + 1) + maxAreaIslandDfs(grid, i - 1,
+                j) + maxAreaIslandDfs(grid, i, j - 1);
+    }
+
+    // * Wall and gates / Islands and treasures
+    public void islandsAndTreasure(int[][] grid) {
+        ArrayDeque<Coordinate> queue = new ArrayDeque<>();
+        int n = grid.length;
+        int m = grid[0].length;
+
+        // Add all the exit points to the queue first
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    queue.offer(new Coordinate(i, j));
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            Coordinate currentCoordinate = queue.poll();
+            List<Coordinate> neighbours = islandAndTreasureGetNeighbours(currentCoordinate, n, m);
+            for (Coordinate neighbour : neighbours) {
+                if (grid[neighbour.r][neighbour.c] == Integer.MAX_VALUE) { // Means not visited
+                    grid[neighbour.r][neighbour.c] = 1 + grid[currentCoordinate.r][currentCoordinate.c]; // Adding 1
+                    // to the current coordinate's value and setting it to the neighbour
+                    queue.add(neighbour);
+                }
+            }
+        }
+    }
+
+    private List<Coordinate> islandAndTreasureGetNeighbours(Coordinate currentCoordinate, int n, int m) {
+        List<Coordinate> neighbours = new ArrayList<>();
+        int[] deltaRow = new int[]{0, 1, 0, -1};
+        int[] deltaCol = new int[]{1, 0, -1, 0};
+
+        for (int i = 0; i < deltaRow.length; i++) {
+            int newRow = currentCoordinate.r + deltaRow[i];
+            int newCol = currentCoordinate.c + deltaCol[i];
+            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m) {
+                neighbours.add(new Coordinate(newRow, newCol));
+            }
+        }
+        return neighbours;
     }
 
 
