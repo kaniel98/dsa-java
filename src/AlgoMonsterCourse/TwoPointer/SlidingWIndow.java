4efc3b7d1dd4866a2061d61eb18a9e1b7d15eac5
Kaniel <kankoh98@gmail.com>
2024-07-12 22:48:51 +0800

    Course: Two pointers

--- src/AlgoMonsterCourse/TwoPointer/SlidingWIndow.java
+++ src/AlgoMonsterCourse/TwoPointer/SlidingWIndow.java
@@ -1,7 +1,29 @@
 package AlgoMonsterCourse.TwoPointer;
 
+import java.util.List;
+
 public class SlidingWIndow {
     public static void main(String[] args) {
-        
+
+    }
+
+    // * Subarray sum fixed
+    // * Time complexity - o(n)
+    // * Space complexity - o(1)
+    public static int subarraySumFixed(List<Integer> nums, int k) {
+        // Note array cannot be sorted, it is fixed
+        int currSum = 0;
+        int left = 0;
+        for (int i = 0; i < k; i++) {
+            currSum += nums.get(i);
+        }
+        int maxSum = currSum;
+        for (int right = k; right < nums.size(); right++) {
+            currSum -= nums.get(left);
+            currSum += nums.get(right);
+            maxSum = Math.max(currSum, maxSum);
+            left++;
+        }
+        return maxSum;
     }
 }
