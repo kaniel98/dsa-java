4884b101915dbcf1f8ad192969836a8c3b07cec6
Kaniel Koh <kankoh98@gmail.com>
2024-09-14 15:08:36 +0800

    621. Task Scheduler

--- src/AlgoMonsterCourse/heap/Heap.java
+++ src/AlgoMonsterCourse/heap/Heap.java
@@ -42,4 +42,46 @@ public class Heap {
 
         return result;
     }
+
+    // 621. Task Scheduler
+    // Time complexity - o(n log n) - log n to insert and pop and repeated n times
+    // Space complexity - o(n)
+    public int leastInterval(char[] tasks, int n) {
+        // Use a max heap to keep track of the count of each character
+        // Everytime we remove the most frequent - The task is executed
+        // Add the character into a queue (To keep track of the idle time before we can add it back)
+        // Keep a time counter to keep track of when the pop the queue
+        if (n == 0) return tasks.length;
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a); // Max heap
+        ArrayDeque<List<Integer>> queue = new ArrayDeque<>(); // Queue
+
+        // Get the count of each character
+        int[] arr = new int[26]; // 26 Characters
+        for (char c : tasks) {
+            arr[c - 'A']++;
+        }
+        
+
+        // Put the count into the heap
+        for (int num : arr) {
+            if (num > 0) maxHeap.add(num);
+        }
+
+        int timer = 0;
+        while (!maxHeap.isEmpty() || !queue.isEmpty()) {
+            if (!maxHeap.isEmpty()) {
+                int currentTask = maxHeap.poll();
+                if (currentTask - 1 > 0) { // If there is more thn 0 of such task left, add it to queue
+                    queue.offer(List.of(currentTask - 1, timer + n));
+                }
+            }
+
+            if (!queue.isEmpty() && queue.peek().get(1) == timer) { // If the first element in the queue is ready to be added back, pop it and add it back to heap
+                maxHeap.add(queue.poll().get(0));
+            }
+            timer++;
+        }
+        return timer;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,8 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
     private String str;
 
     public static void main(String[] args) {
-        System.out.println(markerPlaced(new int[]{-8, 0, 2, 4, 5}, new int[]{-9, 4, 5, 6, 8}));
     }
-
-    public static int markerPlaced(int[] startX, int[] endX) {
-        int answer = 0;
-        // Combine into a single array and sort it
-        int[][] lineSegments = new int[startX.length][];
-        for (int i = 0; i < startX.length; i++) {
-            lineSegments[i] = new int[]{startX[i], endX[i]};
-        }
-        Arrays.sort(lineSegments, Comparator.comparingInt(a -> a[0]));
-
-        int count = 0;
-        for (int i = 0; i < lineSegments.length - 1; i++) {
-            if (i == 0) {
-                continue;
-            }
-            if (lineSegments[i][1] >= lineSegments[i + 1][0]) {
-                count++;
-            } else {
-                answer += count;
-                count++;
-            }
-        }
-        return answer + count;
-    }
-
 }
