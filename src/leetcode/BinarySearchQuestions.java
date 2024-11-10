e990c5df2d87e43f722b66745094d1afc9d27cce
Kaniel Koh <kankoh98@gmail.com>
2024-11-10 18:11:59 +0800

    540.

--- src/leetcode/BinarySearchQuestions.java
+++ src/leetcode/BinarySearchQuestions.java
@@ -527,4 +527,37 @@ public class BinarySearchQuestions {
         // Finding the number closest to the upper bound - Right
         return right;
     }
+
+    // * 540. Single Element in a Sorted Array
+    // * Time complexity: o(log n)
+    // * Space complexity: o(1)
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int boundary = 0;
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 0) {
+                if (mid < nums.length - 1 && nums[mid + 1] == nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    boundary = mid;
+                    right = mid - 1;
+                }
+            } else {
+                if (mid > 0 && nums[mid - 1] == nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    boundary = mid;
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return nums[boundary];
+    }
 }
