156cd2cea0ae1ece218769b646c5a4d1a0e4b565
Kaniel Koh <kankoh98@gmail.com>
2024-11-23 23:32:31 +0800

    1072.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        int[] row = {1, 2, 3, 4};
+        // Join into a string
+        new HashMap<>().getOrDefault()
     }
 }
--- src/leetcode/MatrixQuestion.java
+++ src/leetcode/MatrixQuestion.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MatrixQuestion {
     public static void main(String[] args) {
@@ -23,5 +25,41 @@ public class MatrixQuestion {
         }
 
         return result;
+    }
+
+    // 1072. Flip Columns For Maximum Number of Equal Rows
+    // Time complexity: o(n * m)
+    // Space complexity: o(n)
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        // Find the number of flips required to get the max number of rows with the same values
+        // * It is too expensive to flip every column and check
+        // * Either find rows of the same value or invered values of it
+        // * Inversed row will count because consider the following
+        int max = 0;
+        Map<String, Integer> count = new HashMap<>();
+
+        for (int[] row : matrix) {
+            String rowString = Arrays.toString(row);
+
+            if (row[0] == 0) { // Store only the rows with 1
+                rowString = getInversedValue(row);
+            }
+
+            count.put(rowString, count.getOrDefault(rowString, 0) + 1);
+            max = Math.max(max, count.get(rowString));
+        }
+
+        return max;
+    }
+
+    public String getInversedValue(int[] row) {
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] == 1) {
+                row[i] = 0;
+            } else {
+                row[i] = 1;
+            }
+        }
+        return Arrays.toString(row);
     }
 }
